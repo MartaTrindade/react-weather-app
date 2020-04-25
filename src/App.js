452bb44020cf8e219cf9fdd7ef5efc0aac0c9438
MartaTrindade <60898719+MartaTrindade@users.noreply.github.com>
2020-04-25 15:09:32 +0100

    HTML working on stucture

--- src/App.js
+++ src/App.js
@@ -7,12 +7,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          React Weather App
         </p>
-        <Wheather city="Porto"/>
       </header>
+      <div className="currentWeather">
+        <Wheather city="Porto"/>
+      </div>
+      <h3>HOURLY TEMPERATURE</h3>
+      <div class="forecast">
+        <div class="row weatherForecast" id="hourly-forecast"></div>
+      </div>
+      <br />
+      <h3>DAILY TEMPERATURE</h3>
+      <div class="forecast">
+        <div class="row weatherForecast" id="daily-forecast"></div>
+      </div>
+      <footer>
+        <img src={logo} className="App-logo" alt="logo" />
+        <small>Coded by <a href="https://www.linkedin.com/in/marta-trindade-ap/" className="refLindedin" target="_blank">Marta Trindade</a> {" "}
+          and is <a href="https://github.com/MartaTrindade/react-weather-app" className="refGithub" target="_blank">open-sourced on GitHub</a> {" "}
+          and hosted on <a href="https://www.netlify.com/" className="refHost" target="_blank">Netlify</a>
+        </small>
+      </footer>
     </div>
   );
 }
